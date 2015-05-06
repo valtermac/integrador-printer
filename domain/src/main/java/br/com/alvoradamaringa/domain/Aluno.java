e35ee1100e1cfaabd7a2f9b7b05732d6586ba8df
Valter Machado <valtermac@gmail.com>
2015-05-06 00:07:11 -0300

    Update Aluno.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Aluno.java
@@ -2,6 +2,27 @@ package br.com.alvoradamaringa.domain;
 
 public class Aluno{
 	private long idAluno;
-	private long idPessoa;
-	private String ra;
+	private pessoa idPessoa;
+	private String Ra;
+}
+	private long getidAluno(){
+		return this.idAluno;
+	}
+		private void setIdAluno(long idAluno){
+			this.idAlunoCurso = idAluno;
+		}
+		
+			private pessoa getidPessoa(){
+				return this.idPessoa;
+			}
+				private void setidPessoa(pessoa idPessoa){
+					this.idPessoa = idPessoa;
+				}
+					
+					private String getRa(){
+						return this.ra;
+						}
+							private void setStringRa(ra String Ra){
+								this.String Ra = String Ra;
+							}
 }
